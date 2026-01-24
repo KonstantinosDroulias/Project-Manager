7a2f55815a50de9895999db81bb5696f4b26fc69
Konstantinos Droulias <konstantinosdroulias2@gmail.com>
2026-01-24 04:39:56 +0200

    Base app ready for deployment

--- project/models.py
+++ project/models.py
@@ -1,6 +1,18 @@
 from django.contrib.auth.models import User
 from django.db import models
 
+
+class Project(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+"""
 # --- Helper Models (Now User-Specific) ---
 class Category(models.Model):
     # Added: Each category belongs to a specific user
@@ -95,3 +107,4 @@ class Task(models.Model):
 
     def __str__(self):
         return self.title
+"""
--- project/views.py
+++ project/views.py
@@ -1,28 +1,53 @@
-from django.contrib import messages
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
-
-from project.models import Project
-
-
-# Create your views here.
+from .models import Project
+from .forms import ProjectForm
 
 @login_required
 def index(request):
     projects = Project.objects.filter(user=request.user)
     context = {
-        projects: projects
+        'projects': projects  # Fixed syntax error (added quotes)
     }
     return render(request, 'project/index.html', context)
 
 @login_required
 def create(request):
+    if request.method == 'POST':
+        form = ProjectForm(request.POST)
+        if form.is_valid():
+            project = form.save(commit=False)
+            project.user = request.user
+            project.save()
+            return redirect('home')  # Make sure your URL name for index is 'home'
+    else:
+        form = ProjectForm()
+
     context = {
+        'form': form
     }
     return render(request, 'project/create.html', context)
 
+
 @login_required
-def single(request ,pk):
+def single(request, pk):
+    project = get_object_or_404(Project, pk=pk, user=request.user)
+
+    if request.method == 'POST':
+        if 'delete' in request.POST:
+            project.delete()
+            return redirect('home')
+
+        form = ProjectForm(request.POST, instance=project)
+        if form.is_valid():
+            form.save()
+            return redirect('single-project', pk=pk)
+
+    else:
+        form = ProjectForm(instance=project)
+
     context = {
+        'project': project,
+        'form': form
     }
     return render(request, 'project/single.html', context)
