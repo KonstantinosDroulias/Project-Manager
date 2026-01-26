55650637959add20825f0a71c92a427c386eeecc
Konstantinos Droulias <konstantinosdroulias2@gmail.com>
2026-01-26 14:21:49 +0200

    ENv in the Settings.py

--- config/settings.py
+++ config/settings.py
@@ -16,7 +16,7 @@ import os
 from django.conf.global_settings import CSRF_TRUSTED_ORIGINS
 from dotenv import load_dotenv
 
-load_dotenv('.env.local')
+load_dotenv('.env')
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
